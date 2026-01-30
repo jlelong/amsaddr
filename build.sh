66c3f9de6677546801b5db4b2c75c0c668b5f508
Jerome Lelong <jerome.lelong@univ-grenoble-alpes.fr>
2026-01-30 13:48:43 +0100

    Use -synctex=1

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 latex amsaddr.ins
 pdflatex amsaddr.dtx
 makeindex -s gglo.ist -o amsaddr.gls amsaddr.glo
-pdflatex amsaddr.dtx
-pdflatex amsaddr.dtx
+pdflatex -synctex=1 amsaddr.dtx
+pdflatex -synctex=1 amsaddr.dtx
